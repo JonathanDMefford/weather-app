86afa6ffb51252bccbf32aff2b7de7e9ed31291a
Jonathan Mefford <Jonathanmefford@Justin-iPhone.localdomain>
2020-03-04 16:40:36 -0500

    update files

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-var path = 'https://openweathermap.org/find?q=london';
-const apiKey = 'b19d0b4941d9407a22fb692eeaac3313';
+let path = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+let zip = document.getElementById('zipcode');
+let apiKey = '&appid=b19d0b4941d9407a22fb692eeaac3313';
 let city = document.getElementById('city');
 let tempK = document.getElementById('tempK');
 let tempF = document.getElementById('tempF');
 let tempC = document.getElementById('tempC');
-const url = 'https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=b19d0b4941d9407a22fb692eeaac3313';
+let conditions = document.getElementById('conditions');
 let info;
-function weather() {
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            info = data;
-            city.innerHTML = info.name;
-            tempK.innerHTML = Math.floor(info.main.temp) + ' ' + '<sup>o</sup>K';
-            tempC.innerHTML = Math.floor(info.main.temp - 273) + ' ' + '<sup>o</sup>C';
-            tempF.innerHTML = Math.floor((info.main.temp - 273.15) * (9/5) + 32) + ' ' + '<sup>o</sup>F';
-            console.log(data);
-        })
-        .catch((error) => {
-            console.log('Error', error);
-        })
+//let url = 'https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=b19d0b4941d9407a22fb692eeaac3313';
 
+function run () {
+    if (zip.value.length == 5) {
+        weather(zip);
+    }
 }
-weather();
+
+function weather() {
+    fetch(path + zip.value + apiKey)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        info = data;
+        city.innerHTML = info.name;
+        tempK.innerHTML = Math.floor(info.main.temp) + ' ' + '<sup>o</sup>K';
+        tempC.innerHTML = Math.floor(info.main.temp - 273) + ' ' + '<sup>o</sup>C';
+        tempF.innerHTML = Math.floor((info.main.temp - 273.15) * (9 / 5) + 32) + ' ' + '<sup>o</sup>F';
+        conditions.innerHTML = info.weather[0].description;
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log('Error', error);
+    })
+    
+}
