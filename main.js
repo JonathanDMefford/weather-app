0839bb368cdf6ed269809316245171798a7e13a0
Jonathan Mefford <Jonathanmefford@Justin-iPhone.localdomain>
2020-03-05 11:20:30 -0500

    update files

--- main.js
+++ main.js
@@ -8,34 +8,48 @@ let tempC = document.getElementById('tempC');
 let conditions = document.getElementById('conditions');
 let image = document.getElementById('image');
 let info;
-//let url = 'https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=b19d0b4941d9407a22fb692eeaac3313';
 
+document.getElementById('alert').style.display = 'none';
 document.getElementById('body').style.display = 'none';
 function run () {
-    if (zip.value.length == 5) {
-        weather(zip);
+    if (zip.value.length == 5 && !isNaN(zip.value)) {
+        weather(zip.value);
         document.getElementById('body').style.display = 'block';
+        document.getElementById('alert').style.display = 'none';
+    } else {
+        document.getElementById('body').style.display = 'none';
+        document.getElementById('alert').style.display = 'block';
     }
 }
 
-function weather() {
-    fetch(path + zip.value + apiKey)
+function weather(z) {
+    
+    fetch(path + z + apiKey)
     .then((response) => {
+        
         return response.json();
     })
     .then((data) => {
-        info = data;
-        city.innerHTML = info.name;
-        tempK.innerHTML = Math.floor(info.main.temp) + ' ' + '<sup>o</sup>K';
-        tempC.innerHTML = Math.floor(info.main.temp - 273) + ' ' + '<sup>o</sup>C';
-        tempF.innerHTML = Math.floor((info.main.temp - 273.15) * (9 / 5) + 32) + ' ' + '<sup>o</sup>F';
-        conditions.innerHTML = info.weather[0].description;
-        image.innerHTML = `<img class="h-50" src="http://openweathermap.org/img/w/${info.weather[0].icon}.png">`;
+        if(data.cod == "404"){
+            // I have an error
+            console.log(data.message);
+            document.getElementById('body').style.display = 'none';
+            document.getElementById('alert').style.display = 'block';
+        } else {
+            info = data;
+            city.innerHTML = info.name;
+            tempK.innerHTML = Math.floor(info.main.temp) + ' ' + '<sup>o</sup>K';
+            tempC.innerHTML = Math.floor(info.main.temp - 273) + ' ' + '<sup>o</sup>C';
+            tempF.innerHTML = Math.floor((info.main.temp - 273.15) * (9 / 5) + 32) + ' ' + '<sup>o</sup>F';
+            conditions.innerHTML = info.weather[0].description;
+            image.innerHTML = `<img class="h-50" src="http://openweathermap.org/img/w/${info.weather[0].icon}.png">`;
 
-        console.log(data);
+            console.log(data);
+        }
     })
     .catch((error) => {
-        console.log('Error', error);
+        console.log(error);
     })
     
 }
+
